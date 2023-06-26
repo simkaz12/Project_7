065865edc97c90f819ea0697e0e6f12e16b6d4be
Simonas Kazlauskas <sksimkaz8@gmail.com>
2023-06-26 13:07:41 +0300

    fetch

--- js/index.js
+++ js/index.js
@@ -1,1 +1,3 @@
-console.log('fetch demo...')
+const response = fetch('http://localhost:10002/data/marks.json').then((res) => res.json()).then((data) => {console.log(data)});
+
+console.log(response);
